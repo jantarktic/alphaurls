0ce062ea2a4b72d64b7c4702b01f66c0eab248ad
jantarktic <jaydenclark@outlook.com.au>
2023-03-26 20:39:25 +1100

    added login page and configured supabase

---
+++ src/pages/Signup.js
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import supabase from "../../utils/supabaseClient";
+
+const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function signUpWithEmail() {
+    try {
+      const res = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+      if (res.error) throw res.error;
+      const userId = res.data.user?.id;
+      console.log("userId: ", userId);
+    } catch {
+      console.log("there is an error");
+    }
+  }
+
+  return (
+    <section>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          placeholder="you@example.com"
+          autoComplete="username"
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          minLength="8"
+          required
+          placeholder="********"
+          autoComplete="current-password"
+        />
+
+        <button type="button" onClick={signUpWithEmail}>
+          Sign Up
+        </button>
+      </form>
+    </section>
+  );
+};
+
+export default Signup;
